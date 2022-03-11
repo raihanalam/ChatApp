22ad8ab88e8300d5c957735fa5c1d5189c686166
raihanalam <raihanhridoybd@gmail.com>
2022-03-12 05:55:53 +0600

    deploy 3

--- ChatApp/asgi.py
+++ ChatApp/asgi.py
@@ -6,6 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
+
 import os
 
 from django.core.asgi import get_asgi_application
@@ -23,11 +24,12 @@ application = ProtocolTypeRouter({
 
 
 """
+from .wsgi import *
 import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatApp.Sttings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ChatApp.settings")
 django.setup()
 application = get_default_application()
 """
