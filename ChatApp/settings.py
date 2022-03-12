86b0c8808be7b13c79879868fb516de0fc55b7d2
raihanalam <raihanhridoybd@gmail.com>
2022-03-13 02:11:33 +0600

    deploy test

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -85,7 +85,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts":[os.environ.get('REDIS_URL','redis://loacalhost:6379')]
+            "hosts":[os.environ.get('REDIS_URL','redis://loacalhost:6379')],
+            "hosts":[('127.0.0.1', 6379)],
     
         },
     },
